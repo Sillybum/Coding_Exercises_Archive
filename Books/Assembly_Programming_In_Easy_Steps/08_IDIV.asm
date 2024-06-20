bfbf048deb8b0e6ff83b8de2442b6fddc8be034f
silly_bum <130348695+Sillybum@users.noreply.github.com>
2024-06-20 17:51:44 +0900

    Practice IDIV instruction

---
+++ Books/Assembly_Programming_In_Easy_Steps/08_IDIV.asm
@@ -0,0 +1,21 @@
+INCLUDELIB kernel32.lib
+ExitProcess PROTO
+
+.DATA
+
+.CODE
+main PROC
+XOR RAX, RAX
+XOR RBX, RBX
+XOR RDX, RDX
+MOV RAX, 100
+MOV RBX, 3
+IDIV RBX
+MOV RAX, -100
+CQO
+IDIV RBX
+
+CALL ExitProcess
+main ENDP
+
+END
