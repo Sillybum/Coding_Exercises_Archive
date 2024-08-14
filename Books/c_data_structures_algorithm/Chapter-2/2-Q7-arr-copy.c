9c5eae12ccedb60d6cd085538bfbca220f7f957a
silly_bum <130348695+Sillybum@users.noreply.github.com>
2024-08-14 17:02:12 +0900

    Create 2-Q7-arr-copy.c

---
+++ Books/c_data_structures_algorithm/Chapter-2/2-Q7-arr-copy.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+void ary_copy(int a[], const int b[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        a[i] = b[i];
+    }
+}
+
+int main() {
+    int a[5];
+    int b[] = {1,2,3,4,5};
+    ary_copy(a, b, 5);
+    for(int i=0; i<5; i++)
+        printf("%d", a[i]);
+    return 0;
+}
