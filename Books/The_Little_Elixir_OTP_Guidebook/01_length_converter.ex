43b6e11d5b6ae47cf7bb24b88a3c97472448c37e
silly_bum <130348695+Sillybum@users.noreply.github.com>
2024-08-26 18:08:39 +0900

    Delete Books/The_Little_Elixir_OTP_Guidebook directory

--- Books/The_Little_Elixir_OTP_Guidebook/01_length_converter.ex
+++
@@ -1,5 +0,0 @@
-defmodule MeterToLengthConverter do
-  def convert(:feet, m) when is_number(m) and m >= 0, do: m * 3.28084
-  def convert(:yard, m) when is_number(m) and m >= 0, do: m * 1.09361
-  def convert(:inch, m) when is_number(m) and m >= 0, do: m * 39.3701
-end
